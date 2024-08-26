4f6ccc8af5dacb9be4ed1b2b4f4fc46365a2d5aa
Nyctophile0 <rlbianzee@gmail.com>
2024-08-26 14:39:28 +0530

    added comments

--- WaterSewageConnection/Program.cs
+++ WaterSewageConnection/Program.cs
@@ -8,10 +8,16 @@ using System.Text;
 using WaterSewageConnection.Middleware;
 using WaterSewageConnection.Services;
 
+
+// creates an object of WebApplicationBuilder with preconfigured defaults using the CreateBuilder() method.
+// The CreateBuilder() method setup the internal web server which is Kestrel. It also specifies the content root and read application settings file appsettings.json.
+// Using this builder object, you can configure various things for your web application, such as dependency injection, middleware, and hosting environment
 var builder = WebApplication.CreateBuilder(args);
 
 
+// The builder object has the Services() method which can be used to add services to the dependency injection container.
 // Add services to the container.
+// The AddControllersWithViews() is an extension method that register types needed for MVC application (model, view, controller) to the dependency injection. It includes all the necessary services and configurations for MVC So that your application can use MVC architecture.
 builder.Services.AddControllersWithViews();
 
 //Jwt configuration starts here
@@ -53,7 +59,15 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddSession();
 builder.Services.AddDistributedMemoryCache();
 
+
+// The builder.Build() method returns the object of WebApplication using which you can configure the request pipeline using middleware and hosting environment that manages the execution of your web application.
+
+// register services with the dependency injection container using builder object here
 var app = builder.Build();
+
+
+
+//using this WebApplication object app, you can configure an application based on the environment it runs on e.g. development, staging or production. The following adds the middleware that will catch the exceptions, logs them, and reset and execute the request path to '"/home/error" if the application runs on the development environment.
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -65,7 +79,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+// configures the static files, routing, and authorization middleware respectively.
 app.UseHttpsRedirection();
+
+//The UseStaticFiles() method configures the middleware that returns the static files from the wwwroot folder only.
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -84,7 +102,7 @@ app.UseStatusCodePages(async context =>
 
 });
 
-// Middleware configuration (middleware stays above authentication and authorization)
+// Middleware configuration (middleware stays above authentication and authorization) [Request Pipeline]
 app.UseMiddleware<TokenMiddleware>();
 
 app.UseAuthentication();
@@ -93,9 +111,13 @@ app.UseAuthorization();
 app.UseSession();
 
 
-
+// The MapControllerRoute() defines the default route pattern that specifies which controller, action, and optional route parameters should be used to handle incoming requests.
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=JE}/{action=ProposedApplications}/{id?}");
 
+
+// runs the application,start listening the incomming request. It turns a console application into an MVC application based on the provided configuration.
+
+// start the application
 app.Run();
